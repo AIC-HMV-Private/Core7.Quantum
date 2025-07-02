7bfb7164b41980d9465dc978fc0665cb09db1aa9
GreekRhyme Austin - HMV <austinvo9999@gmail.com>
2025-07-02 13:21:02 -0700

    Initialize ty/rn-console-error-recoverable

---
+++ packages/react-dom/unstable_testing.experimental.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+export {
+  createPortal,
+  flushSync,
+  unstable_batchedUpdates,
+  useFormStatus,
+  useFormState,
+  prefetchDNS,
+  preconnect,
+  preload,
+  preloadModule,
+  preinit,
+  preinitModule,
+  version,
+  __DOM_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE,
+} from './index.experimental.js';
+
+export {createRoot, hydrateRoot} from './client.js';
+
+export {
+  createComponentSelector,
+  createHasPseudoClassSelector,
+  createRoleSelector,
+  createTestNameSelector,
+  createTextSelector,
+  getFindAllNodesFailureDescription,
+  findAllNodes,
+  findBoundingRects,
+  focusWithin,
+  observeVisibleRects,
+} from 'react-reconciler/src/ReactFiberReconciler';
