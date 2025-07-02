fc8136a83a2f82b96c34894be919f132d0323649
GreekRhyme Austin - HMV <austinvo9999@gmail.com>
2025-07-02 14:38:55 -0700

    Initialize pr32661

---
+++ compiler/scripts/release/shared/packages.js
@@ -0,0 +1,11 @@
+const PUBLISHABLE_PACKAGES = [
+  'babel-plugin-react-compiler',
+  'eslint-plugin-react-compiler',
+  'eslint-plugin-react-hooks',
+  'react-compiler-healthcheck',
+  'react-compiler-runtime',
+];
+
+module.exports = {
+  PUBLISHABLE_PACKAGES,
+};
