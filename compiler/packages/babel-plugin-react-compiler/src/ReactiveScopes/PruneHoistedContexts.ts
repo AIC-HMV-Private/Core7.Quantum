a3a8db647db47687d3ac36b4957e35e337254fbf
GreekRhyme Austin - HMV <austinvo9999@gmail.com>
2025-07-02 14:47:11 -0700

    Initialize gh/josephsavona/78/head

---
+++ compiler/packages/babel-plugin-react-compiler/src/ReactiveScopes/PruneHoistedContexts.ts
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {
+  convertHoistedLValueKind,
+  IdentifierId,
+  InstructionKind,
+  ReactiveFunction,
+  ReactiveInstruction,
+  ReactiveScopeBlock,
+  ReactiveStatement,
+} from '../HIR';
+import {empty, Stack} from '../Utils/Stack';
+import {
+  ReactiveFunctionTransform,
+  Transformed,
+  visitReactiveFunction,
+} from './visitors';
+
+/*
+ * Prunes DeclareContexts lowered for HoistedConsts, and transforms any references back to its
+ * original instruction kind.
+ */
+export function pruneHoistedContexts(fn: ReactiveFunction): void {
+  visitReactiveFunction(fn, new Visitor(), {
+    activeScopes: empty(),
+  });
+}
+
+type VisitorState = {
+  activeScopes: Stack<Set<IdentifierId>>;
+};
+
+class Visitor extends ReactiveFunctionTransform<VisitorState> {
+  override visitScope(scope: ReactiveScopeBlock, state: VisitorState): void {
+    state.activeScopes = state.activeScopes.push(
+      new Set(scope.scope.declarations.keys()),
+    );
+    this.traverseScope(scope, state);
+    state.activeScopes.pop();
+  }
+  override transformInstruction(
+    instruction: ReactiveInstruction,
+    state: VisitorState,
+  ): Transformed<ReactiveStatement> {
+    this.visitInstruction(instruction, state);
+
+    /**
+     * Remove hoisted declarations to preserve TDZ
+     */
+    if (instruction.value.kind === 'DeclareContext') {
+      const maybeNonHoisted = convertHoistedLValueKind(
+        instruction.value.lvalue.kind,
+      );
+      if (maybeNonHoisted != null) {
+        return {kind: 'remove'};
+      }
+    }
+    if (
+      instruction.value.kind === 'StoreContext' &&
+      instruction.value.lvalue.kind !== InstructionKind.Reassign
+    ) {
+      /**
+       * Rewrite StoreContexts let/const/functions that will be pre-declared in
+       * codegen to reassignments.
+       */
+      const lvalueId = instruction.value.lvalue.place.identifier.id;
+      const isDeclaredByScope = state.activeScopes.find(scope =>
+        scope.has(lvalueId),
+      );
+      if (isDeclaredByScope) {
+        instruction.value.lvalue.kind = InstructionKind.Reassign;
+      }
+    }
+
+    return {kind: 'keep'};
+  }
+}
